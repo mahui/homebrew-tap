6bb47a553a29f34ca482bb64d89881235c9b9bf3
Pier Release <release@users.noreply.github.com>
2026-07-09 22:54:17 +0800

    pier 2.0.3

--- Casks/pier.rb
+++ Casks/pier.rb
@@ -1,6 +1,6 @@
 cask "pier" do
-  version "2.0.2"
-  sha256 "5b6013f683cf9403fffa699081201625080793e726d4584a14d32155c5575a46"
+  version "2.0.3"
+  sha256 "ceb13712a08d5894d3e347803ef611c24912fc0317a9883547950008849589f2"
 
   url "https://github.com/mahui/pier-dist/releases/download/v#{version}/Pier-#{version}.dmg",
       verified: "github.com/mahui/pier-dist/"
